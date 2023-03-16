ea9bb3bd9ab3af63a86bf61309f2f727e41e7541
tzzcode <tzzcode@163.com>
2023-03-16 14:05:01 +0800

    Delete dist directory

--- dist/assets/index-164d94b7.js
+++
@@ -1,1 +0,0 @@
-import{_ as n}from"./TabBar-48956c6b.js";import{b as o,c as t,d as r,e,F as _}from"./index-316a5a06.js";const i={__name:"index",setup(c){return(s,m)=>{const a=o("van-nav-bar");return t(),r(_,null,[e(a,{title:"消息",fixed:"",placeholder:""}),e(n)],64)}}};export{i as default};
