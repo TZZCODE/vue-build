88111b81247a49e25ebdf060877798951d8205bb
陶志宗 <tzzcode@163.com>
2023-03-20 09:40:25 +0800

    修改

---
+++ assets/index-cd5aa8a1.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./TabBar-14d1b01c.js";import{b as o,c as t,d as r,e,F as _}from"./index-cd82d301.js";const i={__name:"index",setup(c){return(s,m)=>{const a=o("van-nav-bar");return t(),r(_,null,[e(a,{title:"消息",fixed:"",placeholder:""}),e(n)],64)}}};export{i as default};
--- assets/index-d767d4a4.js
+++
@@ -1,1 +0,0 @@
-import{_ as n}from"./TabBar-715b52f0.js";import{b as o,c as t,d as r,e,F as _}from"./index-35dfb947.js";const i={__name:"index",setup(c){return(s,m)=>{const a=o("van-nav-bar");return t(),r(_,null,[e(a,{title:"在线学习",fixed:"",placeholder:""}),e(n)],64)}}};export{i as default};
