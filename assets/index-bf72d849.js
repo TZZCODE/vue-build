8a3c0b2ae28382bb91ef4b6281b4919d758e901e
陶志宗 <tzzcode@163.com>
2023-03-21 14:14:01 +0800

    修改

--- assets/index-bf72d849.js
+++
@@ -1,1 +0,0 @@
-import{_ as n}from"./TabBar-774605d0.js";import{b as o,c as t,d as r,e,F as _}from"./index-9c5adcd1.js";const i={__name:"index",setup(c){return(s,m)=>{const a=o("van-nav-bar");return t(),r(_,null,[e(a,{title:"在线学习",fixed:"",placeholder:""}),e(n)],64)}}};export{i as default};
